0748e66020989d8c0f957f79420e1fdaee8463c7
dayinfinte <zsq941556540@gmail.com>
2017-02-09 11:29:03 +0800

    重新定义用户和主页表单

--- manager.py
+++ manager.py
@@ -3,7 +3,7 @@
 
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, POST
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 from flask_markdown import Markdown 
@@ -15,7 +15,7 @@ migrate = Migrate(app, db)
 markdown = Markdown(app)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+    return dict(app=app, db=db, User=User, POST=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 manager.add_command("runserver", Server(host='127.0.0.1', port=5000, user_debugger=True))
