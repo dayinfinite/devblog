40377e9204d12a2ce450729b3dc9988a62668578
dayinfinte <zsq920409@gmail.com>
2017-02-16 02:47:49 +0800

    前端定义

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 app=Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
